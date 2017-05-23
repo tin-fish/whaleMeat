b01ffcb0e68ff2304868b1b0957dacae78cbd464
tin-fish <mart@tin-fish.com>
2017-05-23 20:21:56 +0100

    Added stop.sh

---
+++ 660devel/stop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+docker stop tinfish06_idx
+docker rm tinfish06_idx
+docker stop tinfish07_idx
+docker rm tinfish07_idx
+docker stop tinfish08_idx
+docker rm tinfish08_idx
+docker stop tinfish09_clm
+docker rm tinfish09_clm
+docker stop tinfish00_dps
+docker rm tinfish00_dps
