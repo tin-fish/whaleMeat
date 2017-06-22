ff8a79490d84ddc36eaa3beffcdb358e54a259a5
Martin Fisher <mafisher@splunk.com>
2017-06-22 15:07:01 +0100

    Renamed 661fwd to unifwd

--- 661fwd/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Running Splunk setup routines"
-/opt/splunkforwarder/bin/splunk start --accept-license
-/opt/splunkforwarder/bin/splunk start && hostname | xargs /opt/splunkforwarder/bin/splunk set servername $1 -auth admin:changeme
-/opt/splunkforwarder/bin/splunk set deploy-poll $SIRE:8089
-/opt/splunkforwarder/bin/splunk restart
-echo "#!/bin/bash" > /start.sh
-echo "/opt/splunkforwarder/bin/start" >> /start.sh
