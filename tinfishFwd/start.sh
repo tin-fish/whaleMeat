99ff388437c31b918217b1bc00ba05258327dc88
Martin Fisher <mafisher@splunk.com>
2017-08-08 10:56:04 +0100

    Added the tinfish forwarder

---
+++ tinfishFwd/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Running Splunk setup routines"
+echo "OPTIMISTIC_ABOUT_FILE_LOCKING = 1" >> /opt/splunkforwarder/etc/splunk-launch.conf
+/opt/splunkforwarder/bin/splunk start --accept-license
+/opt/splunkforwarder/bin/splunk start && hostname | xargs /opt/splunkforwarder/bin/splunk set servername $1 -auth admin:changeme
+/opt/splunkforwarder/bin/splunk install app /splunkclouduf.spl -auth admin:changeme
+/opt/splunkforwarder/bin/splunk restart
+echo "#!/bin/bash" > /start.sh
+echo "/opt/splunkforwarder/bin/start" >> /start.sh
