33a5fc49f3a18998a4025048b0ea9ac59ca2c013
Martin Fisher <mafisher@splunk.com>
2017-09-04 13:30:45 +0100

    Updated hostname fix and PATH

--- cloudUFtester/start.sh
+++ cloudUFtester/start.sh
@@ -3,4 +3,6 @@
 # accepts licence, installs UF and stops - ready for use.
 /opt/splunkforwarder/bin/splunk start --accept-license
 /opt/splunkforwarder/bin/splunk install app /splunkclouduf.spl -auth admin:changeme
+echo "OPTIMISTIC_ABOUT_FILE_LOCKING = 1" >> /opt/splunkforwarder/etc/splunk-launch.conf
+/opt/splunkforwarder/bin/splunk start && hostname | xargs /opt/splunkforwarder/bin/splunk set servername $1 -auth admin:changeme
 /opt/splunkforwarder/bin/splunk stop
